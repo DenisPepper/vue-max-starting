ca327a3eec7ceb874d5734ad4855365bd7b59cde
DenisPepper <thech@yandex.ru>
2024-08-28 23:04:47 +0300

    Добавит файлы нового примера

--- app.js
+++ app.js
@@ -2,28 +2,10 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
 const config = {
   data() {
-    return {
-      counter: 0,
-      name: '',
-    };
-  },
-  methods: {
-    increment() {
-      this.counter += 1;
-    },
-    decrement() {
-      if (this.counter === 0) return;
-      this.counter -= 1;
-    },
-    log(evt) {
-      console.log(evt.target.value);
-    },
-    clear() {
-      this.name = '';
-    },
+    return { goals: [] };
   },
 };
 
 const app = createApp(config);
 
-app.mount('#events');
+app.mount('#user-goals');
