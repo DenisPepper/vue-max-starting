ca410e3b676709b886407a0c912bd6e54df9c044
DenisPepper <thech@yandex.ru>
2024-08-29 21:09:38 +0300

    Добавит пример на условный оператор

--- app.js
+++ app.js
@@ -2,7 +2,25 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
 const config = {
   data() {
-    return { goals: [] };
+    return {
+      goal: '',
+      goals: [],
+    };
+  },
+  methods: {
+    addGoal() {
+      if (!this.goal) return;
+      this.goals.push(this.goal);
+      this.clearGoal();
+    },
+    clearGoal() {
+      this.goal = '';
+    },
+  },
+  computed: {
+    hasGoals() {
+      return this.goals.length > 0;
+    },
   },
 };
 
