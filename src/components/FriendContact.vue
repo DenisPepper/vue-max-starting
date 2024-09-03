61bdeafe662ae53f127bd69c56fa486e58c99ced
DenisPepper <thech@yandex.ru>
2024-09-03 21:02:15 +0300

    Добавит черновик примера использования компонентов

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.firstName }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="showDetails">
+      <li :key="friend.id">{{ friend.phone }}</li>
+      <li>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showDetails: false,
+      friend: {
+        id: '123',
+        firstName: 'Denis',
+        phone: '156489',
+        email: 'email@google.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.showDetails = !this.showDetails;
+    },
+  },
+};
+</script>
