4cb8f5f1673f217089286d55020c076fe9bfbc41
DenisPepper <thech@yandex.ru>
2024-09-09 16:47:02 +0300

    Добавит ссылку на KeepAlive

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,7 @@ import listViews from '/src/friends-list-options.js';
 
 export default {
   // https://vuejs.org/guide/essentials/component-basics.html#dynamic-components
+  // https://vuejs.org/guide/built-ins/keep-alive.html#keepalive
   components: { FriendList, FriendContact, NewFriend },
   data() {
     return {
