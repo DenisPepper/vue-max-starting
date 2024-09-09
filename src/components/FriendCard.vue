3e80084dbad8d557fbdbb15a0a21b293e49928ab
DenisPepper <thech@yandex.ru>
2024-09-09 11:58:22 +0300

    Добавит черновик компонента со Слотом

---
+++ src/components/FriendCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- slots technic: https://vuejs.org/guide/components/slots.html -->
+  <!-- похоже, что это аналог react props.children -->
+  <div class="friend-card-container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.friend-card-container {
+  display: flex;
+}
+</style>
