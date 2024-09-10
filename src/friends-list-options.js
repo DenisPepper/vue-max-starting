bde4da267aeb835695636c384a558632617aa007
DenisPepper <thech@yandex.ru>
2024-09-10 10:12:41 +0300

    Подготовит черновик нового проекта

--- src/friends-list-options.js
+++
@@ -1,4 +0,0 @@
-export default {
-  row: 'ROW',
-  rowReverse: 'ROW_REVERSE',
-};
--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import FriendContact from './components/FriendContact.vue';
-import NewFriend from './components/NewFriend.vue';
 
 const app = createApp(App);
-
-// this is a global registartion of componets
-// https://vuejs.org/guide/components/registration.html
-
-// эти компоненты бужут загружаться локально
-// app.component('friend-contact', FriendContact);
-// app.component('new-friend', NewFriend);
-
 app.mount('#app');
