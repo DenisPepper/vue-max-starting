b86ca58bbdf9b26faa7cc4e11d0963f510539f69
DenisPepper <thech@yandex.ru>
2024-09-10 17:26:34 +0300

    Добавит компонент модального окна

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import BaseCard from '/src/components/ui/BaseCard.vue';
 import BaseButton from '/src/components/ui/BaseButton.vue';
+import BaseModal from '/src/components/ui/BaseModal.vue';
 
 const app = createApp(App);
 app.component('BaseCard', BaseCard);
 app.component('BaseButton', BaseButton);
+app.component('BaseModal', BaseModal);
 app.mount('#app');
